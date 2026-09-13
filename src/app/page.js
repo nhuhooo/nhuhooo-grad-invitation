c57e3211eaca5f68588bee5f8636507b0fd62bfe
ho nhu <honhu@hookyeo-2.local>
2026-09-13 22:56:54 +0700

    Increase letter spacing for recipient name

--- src/app/page.js
+++ src/app/page.js
@@ -126,7 +126,7 @@ export default async function Home({ searchParams }) {
             {/* Dear section */}
             <div className="flex flex-col md:flex-row items-center md:items-baseline">
               <span className={`${windSongFont.className} text-lg sm:text-xl md:text-2xl lg:text-3xl mr-2`}>To </span>
-              <span className={`${windSongFont.className} text-lg sm:text-xl md:text-2xl lg:text-3xl text-black font-bold mt-1 md:mt-0 md:ml-2 tracking-normal whitespace-pre-wrap`}>
+              <span className={`${windSongFont.className} text-lg sm:text-xl md:text-2xl lg:text-3xl text-black font-bold mt-1 md:mt-0 md:ml-2 tracking-widest whitespace-pre-wrap`}>
                 {to},
               </span>
             </div>
